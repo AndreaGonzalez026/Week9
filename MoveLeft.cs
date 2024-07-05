7eb475ce963025d00eb804348c79c8224b84a425
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-07-04 19:05:23 -0600

    Update MoveLeft.cs

--- MoveLeft.cs
+++ MoveLeft.cs
@@ -8,16 +8,18 @@ private Vector3 spawnPos = new Vector3(25, 0, 0);
 
 {
    private float speed = 30;
+   private PlayerController playerControllerScript; 
    
    // Start is called before the first frame update
    void Start()
    {
-  Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+  playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
    }
 
    //Update is called once per frame
    void Update()
    {
+      if (playerControllerScript.gameOver == false)
       transform.Translate(Vector3.left * Time.deltaTime * speed);
    }
 }
